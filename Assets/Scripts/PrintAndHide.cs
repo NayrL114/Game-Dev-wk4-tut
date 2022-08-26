42d5817588b8e95957379c06a27148cb3571b990
NayrL <40843838+NayrL114@users.noreply.github.com>
2022-08-26 14:53:14 +1000

    90 percent real done

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PrintAndHide: MonoBehaviour
 {
+<<<<<<< Updated upstream
 
     public string objName;
+=======
+    
+>>>>>>> Stashed changes
     public int i;
     public Renderer rend;
 
@@ -14,12 +18,38 @@ public class PrintAndHide: MonoBehaviour
     void Start()
     {
         i = 3;
+<<<<<<< Updated upstream
+=======
+        blueHide = Random.Range(200, 250);// generate a random int between 200 to 250
+        
+        if (this.tag == "Blue")// print out the real value for blue ball to deactivate
+        {
+            Debug.Log("blueHide is " + blueHide);
+        }
+        
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
         i++;
+<<<<<<< Updated upstream
         Debug.Log(objName + i);
+=======
+        Debug.Log(this.name + ":" + i);// print out the required name and frame. Format (objName: i);
+        
+        if (this.tag == "Red" & i == 100){// deactivate read ball on 100th frame
+            //this.SetActive(false); 
+            //GameObject.Find("Red").SetActive(false);
+            //rend.SetActive(false);
+        }
+        
+        if (this.tag == "Blue" & i == blueHide){// deactive blue ball on "blueHide"th frame
+            //this.SetActive(false);
+            //GameObject.Find("Blue").SetActive(false);
+        }
+        
+>>>>>>> Stashed changes
     }
 }
